d1d3f57b494f1754c01cd9868f9af4fe49245f22
jojotheboomboy <heyits.shada@gmail.com>
2021-04-12 13:44:18 -0500

    Implemented Algorithm

--- Piano_Chord_Recognition.py
+++ Piano_Chord_Recognition.py
@@ -1,4 +1,69 @@
-# creates an empy list which will have the x, y, and z values added to them 
+def chordReader(x, y, z, final_list):
+    # value is the sum of x, y and z's indices
+    value = final_list.index(x) + final_list.index(y) + final_list.index(z)
+
+    # This calculates the lowest note being played 'A' being the local minimum and 'G#' being the local maximum
+    if organizer.index(x) < organizer.index(y) and organizer.index(x) < organizer.index(z):
+        leadingNoteLow = x
+    if organizer.index(y) < organizer.index(x) and organizer.index(y) < organizer.index(z):
+        leadingNoteLow = y
+    if organizer.index(z) < organizer.index(x) and organizer.index(z) < organizer.index(y):
+        leadingNoteLow = z
+
+    # This calculates the highest note being played 'A' being the local minimum and 'G#' being the local maximum
+    if organizer.index(x) > organizer.index(y) and organizer.index(x) > organizer.index(z):
+        leadingNoteHigh = x
+    if organizer.index(y) > organizer.index(x) and organizer.index(y) > organizer.index(z):
+        leadingNoteHigh = y
+    if organizer.index(z) > organizer.index(x) and organizer.index(z) > organizer.index(y):
+        leadingNoteHigh = z
+
+    # This calculates the middle note out of the three being played
+    if (organizer.index(x) < organizer.index(y) and organizer.index(x) > organizer.index(z))\
+       or organizer.index(x) > organizer.index(y) and organizer.index(x) < organizer.index(z):
+        leadingNoteMid = x
+    if (organizer.index(y) < organizer.index(x) and organizer.index(y) > organizer.index(z))\
+       or organizer.index(y) > organizer.index(x) and organizer.index(y) < organizer.index(z):
+        leadingNoteMid = y
+    if (organizer.index(z) < organizer.index(x) and organizer.index(z) > organizer.index(y))\
+        or organizer.index(z) > organizer.index(x) and organizer.index(z) < organizer.index(y):
+        leadingNoteMid = z
+    k = organizer.index(leadingNoteHigh) - organizer.index(leadingNoteLow)   
+
+    if (value == 11 or value == 14):
+        if ((organizer.index(leadingNoteHigh) - organizer.index(leadingNoteMid)) == 4):
+            print("{} Major Chord".format(leadingNoteMid))
+        elif ((organizer.index(leadingNoteHigh) - organizer.index(leadingNoteMid)) == 3):
+            print("{} Major Chord".format(leadingNoteLow))
+        else:
+            print("{} Major Chord".format(leadingNoteHigh))
+            
+    elif value == 10 or value == 13:
+        if ((organizer.index(leadingNoteHigh) - organizer.index(leadingNoteMid)) == 4):
+            print("{} minor chord".format(leadingNoteLow))
+        elif ((organizer.index(leadingNoteHigh) - organizer.index(leadingNoteMid)) == 3):
+            print("{} Minor Chord".format(leadingNoteMid))
+        else:
+            print("{} Minor Chord".format(leadingNoteHigh))
+            
+    elif value == 12:
+        if final_list.index(y) == 4:
+            print("{} Augmented Chord".format(leadingNoteLow))
+        else:
+            print("{} Diminished Chord".format(leadingNoteMid))
+            
+    elif value == 9 or value == 15:
+        if ((organizer.index(leadingNoteHigh) - organizer.index(leadingNoteMid)) == 3) and\
+           ((organizer.index(leadingNoteHigh) - organizer.index(leadingNoteLow)) == 6):
+            print ("{} Diminished Chord".format(leadingNoteLow))
+        elif ((organizer.index(leadingNoteHigh) - organizer.index(leadingNoteMid)) == 3) and\
+             ((organizer.index(leadingNoteHigh) - organizer.index(leadingNoteLow)) == 9):
+            print("{} Diminished Chord".format(leadingNoteMid))
+        else:
+            print("{} Diminished Chord".format(leadingNoteHigh))
+
+
+# creates an empy list whGich will have the x, y, and z values added to them 
 my_list = []
 
 # Asks for user input for which key they would like to play and sets them equal to a respective variable
@@ -13,65 +78,69 @@ my_list.append(y)
 my_list.append(z)
 
 # Example of what 'my_list' should look like at this point "['F', 'G', 'D']"
-print(my_list)
+# print(my_list)
 
 # This appends every key that is in an octive
 my_list.append("C")
-my_list.append("C_Sp")
+my_list.append("C#")
 my_list.append("D")
-my_list.append("D_Sp")
+my_list.append("D#")
 my_list.append("E")
 my_list.append("F")
-my_list.append("F_Sp")
+my_list.append("F#")
 my_list.append("G")
-my_list.append("G_Sp")
+my_list.append("G#")
 my_list.append("A")
-my_list.append("A_Sp")
+my_list.append("A#")
 my_list.append("B")
 
 # Example of what 'my_list' should look like at this point is listed in the line below
-# '['F', 'G', 'D', 'C', 'C_Sp', 'D', 'D_Sp', 'E', 'F', 'F_Sp', 'G', 'G_Sp', 'A', 'A_Sp', 'B']'
-print(my_list)
+# '['F', 'G', 'D', 'C', 'C#', 'D', 'D#', 'E', 'F', 'F#', 'G', 'G#', 'A', 'A#', 'B']'
+# print(my_list)
 
 # At this point we take a set of 'my list', this insures that the variables (x, y, z) will still
 # be present in the set, the excess values will be thrown out as a set can only contain unique values
 conversion = set(my_list)
 
 # Example of what 'conversion' should look like at this point is listed in the line below
-# '{'B', 'A', 'D_Sp', 'G', 'A_Sp', 'E', 'C_Sp', 'F_Sp', 'D', 'F', 'C', 'G_Sp'}'
-print(conversion)
+# '{'B', 'A', 'D#', 'G', 'A#', 'E', 'C#', 'F#', 'D', 'F', 'C', 'G#'}'
+# print(conversion)
 
 # this simply organizes 'coversion' in alphabetical order, it also simultaneously converts the set back into a list
 organizer = sorted(conversion)
 
 # Example of what 'organizer' should look like at this point is listed in the line below
-# '['A', 'A_Sp', 'B', 'C', 'C_Sp', 'D', 'D_Sp', 'E', 'F', 'F_Sp', 'G', 'G_Sp']'
-print(organizer)
+# '['A', 'A#', 'B', 'C', 'C#', 'D', 'D#', 'E', 'F', 'F#', 'G', 'G#']'
+# print(organizer)
 
 # These two lists are simply place holder lists which will be used to organize the list in a manner according to the first input
-list1 = []
+final_list = []
 list2 = []
+##organizer = {a,b,}
+##for i in range(0,12):
+##    final_list.append(organizer[(organizer.index(x) + i) % 12])
 
 # This will break the current filled list 'organizer' in to two seperate lists. One list will befilled with the str values which indeces is equal to or greater than
 # the initial x variables index within the list. The other list will be made up of those points which did not meet the given criteria.
 for i in range(len(organizer)):
     if i >= organizer.index(x):
-        list1.append(organizer[i])
+        final_list.append(organizer[i])
     else:
         list2.append(organizer[i])
 
-# Example of what 'list1' should look like at this point is listed in the line below
-# '['F', 'F_Sp', 'G', 'G_Sp']'
-print(list1)
+# Example of what 'final_list' should look like at this point is listed in the line below
+# '['F', 'F#', 'G', 'G#']'
+# print(final_list)
 
 # Example of what 'list2' should look like at this point is listed in the line below
-# '['A', 'A_Sp', 'B', 'C', 'C_Sp', 'D', 'D_Sp', 'E']'
-print(list2)
+# '['A', 'A#', 'B', 'C', 'C#', 'D', 'D#', 'E']'
+# print(list2)
 
-# This algorithm will simply append each value in 'list2' the end of 'list1'
+# This algorithm will simply append each value in 'list2' the end of 'final_list'
 for i in range(len(list2)):
-    list1.append(list2[i])
+    final_list.append(list2[i])
 
-# Example of what 'list1' should look like at this point is listed in the line below
-# ['F', 'F_Sp', 'G', 'G_Sp', 'A', 'A_Sp', 'B', 'C', 'C_Sp', 'D', 'D_Sp', 'E']
-print(list1)        
+# Example of what 'final_list' should look like at this point is listed in the line below
+# ['F', 'F#', 'G', 'G#', 'A', 'A#', 'B', 'C', 'C#', 'D', 'D#', 'E']
+# print("Final list {}".format(final_list))
+chordReader(x, y, z, final_list)
