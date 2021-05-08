26126cd8096567c0ef026906ccf2cb54af6aecad
BluJayB <80533553+BluJayB@users.noreply.github.com>
2021-05-08 16:49:50 -0500

    final 1.0

--- pygame_gui.py
+++ pygame_gui.py
@@ -1,4 +1,5 @@
 import sys, pygame
+from pygame_button import Button
 
 pygame.init()
 pygame.font.init()
@@ -13,6 +14,12 @@ WHITE = 255, 255, 255
 BLACK = 0, 0, 0
 GREY = 130, 130, 130
 screen.fill(WHITE)
+
+BUTTON_STYLE = {
+    "hover_color":GREY,
+    "clicked_color":(70, 70, 70)
+}
+
 #######################3
 import RPi.GPIO as GPIO
 from time import sleep
@@ -31,15 +38,87 @@ my_list = ['A', 'A#', 'B', 'C', 'C#', 'D', 'D#', 'E', 'F', 'F#', 'G', 'G#']
 GPIO.setup(keys, GPIO.IN, GPIO.PUD_DOWN)
 
 piano = Piano()
+octive = 2
+
+def changeOctUp():
+    global octive
+    print("Octive: {}".format(octive))
+    if (octive > 2):
+        pass
+    else:
+        octive += 1
+        piano.changeOctive(octive)
+
+def changeOctDown():
+    global octive
+    print("Octive: {}".format(octive))
+    if (octive < 1):
+        pass
+    else:
+        octive += -1
+        piano.changeOctive(octive)
 
 
+up_button = Button((0, 0, 100, 50), (255, 0, 0), changeOctUp, text="up", **BUTTON_STYLE)
+up_button.rect.center = (width//4+width//2+width//8, 100)
+
+down_button = Button((0, 0, 100, 50), (255, 0, 0), changeOctDown, text="down", **BUTTON_STYLE)
+down_button.rect.center = (width//4+width//2-width//8, 100)
 
 bg = pygame.image.load("SEXY_RECTANGLES.gif")
+
+note_img = pygame.image.load("Whole note.png")
+sharp_img = pygame.image.load("Whole_note_Sharp.png")
+bar = pygame.image.load("Empty_Bar.gif")
+
+def blitNotes():
+    if (down[0]):
+        A = note_img
+        #screen.blit(A, (200, 150))
+        screen.blit(A, (200, 143))
+    if (down[1]):
+        As = sharp_img
+        screen.blit(As, (220, 143))
+    if (down[2]):
+        B = note_img
+        screen.blit(B, (200, 133))
+    if (down[3]):
+        C = note_img
+        screen.blit(C, (200, 123))
+    if (down[4]):
+        Cs = sharp_img
+        screen.blit(Cs, (220, 123))
+    if (down[5]):
+        D = note_img
+        screen.blit(D, (200, 113))
+    if (down[6]):
+        Ds = sharp_img
+        screen.blit(Ds, (220, 113))
+    if (down[7]):
+        E = note_img
+        screen.blit(E, (200, 103))
+    if (down[8]):
+        F = note_img
+        screen.blit(F, (200, 93))
+    if (down[9]):
+        Fs = sharp_img
+        screen.blit(Fs, (220, 93))
+    if (down[10]):
+        G = note_img
+        screen.blit(G, (200, 83))
+    if (down[11]):
+        Gs = sharp_img
+        screen.blit(Gs, (220, 83))
+
+##def event_loop():
+##    pass
 
 while (True):
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             sys.exit()
+        up_button.check_event(event)
+        down_button.check_event(event)
     
     ##screen.fill(background)
     screen.blit(bg,(0,0))
@@ -92,5 +171,17 @@ while (True):
     screen.blit(temp_note, (width//2 + width//4 - 20, height/2+height/4-height//9))
     note_title = leFont.render("Note Recognition", False, WHITE)
     screen.blit(note_title, (width//2 + width//4 - 70, height/2+height/4-height//5))
+    up_button.update(screen)
+    down_button.update(screen)
+    octive_text = leFont.render(str(octive), False, BLACK)
+    screen.blit(octive_text, (width//2+width//4, 150))
     
+    octive_title = leFont.render("Octive", False, WHITE)
+    screen.blit(octive_title, (width//2+width//4-30, 30))
+
+    notation_title = leFont.render("Notation", False, WHITE)
+    screen.blit(notation_title, (width//2 - width//4 - 50, 30))
+    screen.blit(bar, (100, 70))
+    blitNotes()
+        
     pygame.display.flip()
